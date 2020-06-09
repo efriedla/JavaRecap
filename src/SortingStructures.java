72ff28315546285af259537eb52d32d811f57314
efriedla <efriedla20852@gmail.com>
2020-06-08 21:06:25 -0700

    planning Sorting file

---
+++ src/SortingStructures.java
@@ -0,0 +1,13 @@
+public class SortingStructures {
+	public static void main(String[] args){
+		//create an int array(50) with random numbers
+		//print array
+		//have several sorts to choose from
+		//Linear
+		//bubble
+		//insertionSort
+		//selection sort
+		//merge sort
+		//quick sort
+	}
+}
