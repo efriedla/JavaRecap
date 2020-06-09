b5bf674204550165ddab03976e1ce615bcf53261
efriedla <efriedla20852@gmail.com>
2020-06-08 21:15:26 -0700

    planning Sorting file

--- src/SortingStructures.java
+++ src/SortingStructures.java
@@ -1,13 +1,22 @@
+/**
+ * Sorting
+ * create an int array(50) with random numbers
+ * print array
+ * have several sorts to choose from
+ * Linear
+ * bubble
+ * insertionSort
+ * selection sort
+ * merge sort
+ * quick sort
+ * @author EF
+ * @version 1
+ * @throw file does not exists
+ */
+
+
 public class SortingStructures {
 	public static void main(String[] args){
-		//create an int array(50) with random numbers
-		//print array
-		//have several sorts to choose from
-		//Linear
-		//bubble
-		//insertionSort
-		//selection sort
-		//merge sort
-		//quick sort
+
 	}
 }
