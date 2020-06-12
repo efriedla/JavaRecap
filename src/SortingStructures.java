28ede6015db71bb1adb5ca5164d3046f3af93ec9
efriedla <efriedla20852@gmail.com>
2020-06-12 16:10:24 -0700

    Create random array of Bytes

--- src/SortingStructures.java
+++ src/SortingStructures.java
@@ -13,13 +13,18 @@
  * @version 1
  * @throw file does not exists
  */
-
+import java.util.Random;
 
 public class SortingStructures {
 	public static void main(String[] args){
 
+		Random random = new Random();
 
-		System.out.println("Hello World!");
+		byte[] arr = new byte[5];
+		random.nextBytes( arr );
+		for(int i = 0; i < arr.length; i++){
+			System.out.println(arr[i]);
+		}
 	}
 	/**
 	 * Create/populate array
