eca435f54f421670ca8d83c19231e77ec78f4b64
moocow <efriedla20852@gmail.com>
2021-03-31 10:40:35 -0400

    Added method to read time left from secounds

--- src/BasicsRecapVeriables.java
+++ src/BasicsRecapVeriables.java
@@ -18,6 +18,9 @@ public class BasicsRecapVeriables {
 		int placeholder = scan.nextInt();
 		System.out.print("Area of a circle with a radius of " + placeholder + " is ");
 		System.out.format("%.3f%n",circleArea(placeholder));
+
+		// remaining times from seconds
+		minutesLeft(scan);
 		scan.close();
 	}
 
@@ -30,6 +33,18 @@ public class BasicsRecapVeriables {
 		double circumference =  Math.pow(radius,2);
 		double circleArea = Math.PI * circumference;
 		return circleArea;
+	}
+	/**
+	 * Write a program that calculates minutes and remaining seconds from total seconds.
+	 * @param  scan
+	 * @return null prints formated time left
+	 */
+	public static void minutesLeft(Scanner scan){
+		System.out.println("Please type the amount of seconds you would like formatted ");
+		int seconds = scan.nextInt();
+		int minutes = seconds/ 60;
+		int sec = seconds % 60;
+		System.out.println(minutes + " minutes and " + sec + " Seconds");
 	}
 
 }
