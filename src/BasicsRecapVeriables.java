011d0c9ec916c7cec66993f700589dd15e00ba6c
moocow <efriedla20852@gmail.com>
2021-03-31 09:44:56 -0400

    "Setting up for basic reacp"

---
+++ src/BasicsRecapVeriables.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+/**
+ * Problem: Computing the Area of a Circle
+ * This program computes the area of the circle.
+ * As I work through the slides I will add multiple methods to this page
+ * This page will only include the basics
+ *
+ * @version 11
+ * @auther Elizabeth Friendland
+ */
+public class BasicsRecapVeriables {
+	public static void main(String [] args) throws IOException {
+		System.out.println("Setting up for Perscholas ");
+	}
+
+}
