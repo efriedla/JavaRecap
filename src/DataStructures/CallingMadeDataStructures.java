fc4b3ef7a819eb23d2a20984fb129051de5ccfea
moocow <efriedla20852@gmail.com>
2021-04-07 11:33:43 -0400

    ArrayList = resizable array

---
+++ src/DataStructures/CallingMadeDataStructures.java
@@ -0,0 +1,15 @@
+package DataStructures;
+import java.util.ArrayList;
+
+public class CallingMadeDataStructures {
+	public static void main(String[] args) {
+		//ArrayList = resizable array
+		ArrayList<String> cars = new ArrayList<String>(); // Create an ArrayList object
+		//add to arraylist
+		cars.add("Volvo");
+		cars.add("BMW");
+		cars.add("Ford");
+		cars.add("Mazda");
+		System.out.println(cars);
+	}
+}
