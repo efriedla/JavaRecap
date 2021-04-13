77860102a44e0d30e91c2153401d7f1ca6c82bb4
moocow <efriedla20852@gmail.com>
2021-04-13 10:14:21 -0400

    Reviewing lamba with Java Brains on youtube

---
+++ src/Lambda/main.java
@@ -0,0 +1,11 @@
+package Lambda;
+
+/**
+ * Review Lamba with https://www.youtube.com/playlist?list=PLqq-6Pq4lTTa9YGfyhyW2CqdtW9RtY-I3 playlist
+ */
+
+public class main {
+	public static void main(String[] args) {
+
+	}
+}
