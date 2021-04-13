dfe799eb6e62f85bd691ff6031f6ddca80032cc2
moocow <efriedla20852@gmail.com>
2021-04-12 21:28:20 -0400

    finished getters and setters for rest of ingrediance

--- src/CoffeeMachine/Order.java
+++ src/CoffeeMachine/Order.java
@@ -3,7 +3,6 @@ package CoffeeMachine;
 public class Order{
 	int milkInventory;
 	int waterInventory;
-	int cupsInventory;
 	int beansInventory;
 	int cupsOfCoffee;
 	String type;
@@ -13,33 +12,80 @@ public class Order{
 
 	public Order(){
 		setMilkInventory(5000);
+		setBeansInventory(5000);
+		setWaterInventory(5000);
 		this.cupsOfCoffee = 1;
 		this.type = "coffee";
-		setBeansInventory(5000);
+
 	}
 	public Order(int cupsOfCoffee){
 		this.cupsOfCoffee = cupsOfCoffee;
 		setMilkInventory(5000);
 		setBeansInventory(5000);
+		setWaterInventory(5000);
+		this.type = "coffee";
 	}
+
+	public Order(int cupsOfCoffee, String type){
+		this.cupsOfCoffee = cupsOfCoffee;
+		setMilkInventory(5000);
+		setBeansInventory(5000);
+		setWaterInventory(5000);
+		this.type = type;
+	}
+
+	/**
+	 * Milk Inventory Setter
+	 * @param milkInventory
+	 */
 	public void setMilkInventory(int milkInventory){
 		this.milkInventory = milkInventory;
 		System.out.println("milk " + milkInventory + "ml in inventory");
 	}
+
+	/**
+	 * Subtrack milk order from Inventory
+	 */
 	public void useMilk(){
 		this.milkInventory = milkInventory - (cupsOfCoffee * milk_per_cup);
 		System.out.println(cupsOfCoffee * milk_per_cup + " Used milk "+milkInventory + "ml left");
 	}
+
+	/**
+	 * Bean Inventory setter
+	 * @param beansInventory
+	 */
 	public void setBeansInventory(int beansInventory){
 		this.beansInventory = beansInventory;
 	}
+
+	/**
+	 * Subtrack bean usage from inventory
+	 */
 	public void useBeans(){
 		this.beansInventory = beansInventory - (cupsOfCoffee * beans_per_cup);
 		System.out.println(cupsOfCoffee * beans_per_cup + " Used beas "+beansInventory + "ml left");
 	}
 
+	/**
+	 * Set water Inventory
+	 * @param waterInventory
+	 */
+	public void setWaterInventory(int waterInventory){
+		this.waterInventory = waterInventory;
+	}
 
-	//order
+	/**
+	 * Subtrack water used from inventory
+	 */
+	public void useWater(){
+		this.waterInventory -= (cupsOfCoffee * water_per_cup);
+	}
+
+
+	/**
+	 * Display coffee order
+	 */
 	public void coffeeProcess(){
 		System.out.println("Starting to make a coffee");
 		System.out.println("Grinding coffee beans");
@@ -49,26 +95,5 @@ public class Order{
 		System.out.println("Coffee is ready!");
 	}
 
-	public void measureIngrediance() {
 
-
-	}
-
-
-	public void makeCoffee() {
-
-	}
-
-
-	public void charge() {
-
-	}
-
-	public void displayBalance() {
-
-	}
-
-	public void displayIngrediance() {
-
-	}
 }
