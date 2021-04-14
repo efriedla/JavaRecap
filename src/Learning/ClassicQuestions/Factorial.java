e9effeb8be9ceb611e1257d4dd8e76ee790a3643
moocow <efriedla20852@gmail.com>
2021-04-14 19:50:59 -0400

    Adding print statments

--- src/Learning/ClassicQuestions/Factorial.java
+++ src/Learning/ClassicQuestions/Factorial.java
@@ -2,15 +2,48 @@ package Learning.ClassicQuestions;
 
 public class Factorial {
 	public static void main(String args[]){
-		System.out.println(55);
+
 		int n = 5;
 		factorial(n);
+
+//		for(int i = 1; i <= n; i++){
+//			for(int j =1; j <= i; j++){
+//				for(int q = 1; q <=j; q++){
+//					System.out.println(i + " " + j + " "+ q);
+//				}
+//			}
+//		}
+
+		printhafTriangle(n);
+		printhafTriangleUpsideDown(n);
 	}
 	public static void factorial(int n){
-		int i,fact =1;
-		for(i = 1; i <= n; i++){
-			fact *=i;
+		if(n < 0){
+			System.out.println("Sorry can't factorial 0 or negitives");
+		}else {
+			int i, fact = 1;
+			for (i = 1; i <= n; i++) {
+				fact *= i;
+			}
+			System.out.println("Factorial of " + n + " is: " + fact);
 		}
-		System.out.println("Factorial of "+ n +" is: "+fact);
+	}
+	public static void printhafTriangle(int n){
+		int i,j;
+		for(i =1; i<=n; i++){
+			for(j =1;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	public static void printhafTriangleUpsideDown(int n){
+		int i,j;
+		for(i = 1; i<=n; i++){
+			for(j =n;j>=i;j--){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 }
